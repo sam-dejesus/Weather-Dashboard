670bcbae166f6259e914beb23098d222741cdd93
samuel de jesus <samue@MacBook-Air.local>
2023-03-29 16:03:50 -0400

    test

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,8 @@ var citysBtn = $('#btns');
 searchBtn.click(function go(){
  var city = $('#city-search').val();   
 var url = "http://api.openweathermap.org/geo/1.0/direct?q= "+ city + "&limit=1&appid=78da9311b6649705d4159cf6966655ff"
-newBtn= $("<button>").text(city);
-citysBtn.append(newBtn);
+
+
 
 fetch(url)
 .then(response => response.json())
@@ -15,7 +15,31 @@ fetch(url)
  var {lat, lon } = data[0]
  var latValue = lat;
  var lonValue = lon;
-    console.log(latValue, lonValue);
+ var weather = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latValue + "&lon=" + lonValue + "&units=imperial&cnt=6&appid=78da9311b6649705d4159cf6966655ff"
+fetch(weather)
+.then(response => response.json())
+.then(data => { console.log(data);
+    var cityName = data.city.name
+    var temperature = data.list[0].main.temp;
+    var windspeed = data.list[0].wind.speed;
+    var humidity = data.list[0].main.humidity;
+
+
+    console.log(temperature , cityName , windspeed , humidity);
+    
+var pastCityBtn = $("<button>").text(city)
+citysBtn.append(pastCityBtn)
+pastCityBtn.click(() => weatherbtn(latValue, lonValue))
+})
+})
 })
 
-})
+function weatherbtn(lat, lon){
+    var weather = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=78da9311b6649705d4159cf6966655ff";
+    fetch(weather)
+    .then((response => response.json()))
+    .then(data => {
+        console.log(data)
+    })
+}
+
