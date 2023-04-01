49155f26de840ec3109eeefc90b03d040d1fdd43
samuel de jesus <samue@MacBook-Air.local>
2023-04-01 12:56:12 -0400

    added a for loop to cycle through the apis array of dates and was able to get the icon for the weather to show up and connected that function with the buttons so when the new buttons are clicked you will be able to reload the data

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 var searchBtn = $('#search-btn');
 var citysBtn = $('#btns');
-
+var nameData = $('#nameofcity')
+var windData = $('#windspeedofcity')
+var tempData = $('#tempofcity')
+var humidData = $('#humidityofcity')
+var iconData = $('#icons')
+var forecast = $('#forecast')
 
 
 searchBtn.click(function go(){
@@ -18,14 +23,38 @@ fetch(url)
  var weather = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latValue + "&lon=" + lonValue + "&units=imperial&cnt=6&appid=78da9311b6649705d4159cf6966655ff"
 fetch(weather)
 .then(response => response.json())
-.then(data => { console.log(data);
+.then(data => { //console.log(data);
+    forecast.empty()
+    for (var i = 1; i <= 5; i++){
+    var icon = data.list[i].weather[0].icon;
     var cityName = data.city.name
-    var temperature = data.list[0].main.temp;
-    var windspeed = data.list[0].wind.speed;
-    var humidity = data.list[0].main.humidity;
+    var temperature = data.list[i].main.temp;
+    var windspeed = data.list[i].wind.speed;
+    var humidity = data.list[i].main.humidity;
+    var h3 = $('<h3>').text(cityName)
+    var p1 = $('<p>').text(temperature)
+    var p2 = $('<p>').text(windspeed)
+    var p3 = $('<p>').text(humidity)
+    var img = $('<img>').attr("src", "http://openweathermap.org/img/wn/" + icon + ".png")
+    var div = $('<div>').addClass("forecast-day").append(h3, img, p1, p2, p3)
+    forecast.append(div)
 
 
-    console.log(temperature , cityName , windspeed , humidity);
+
+
+
+iconData.attr("src", "http://openweathermap.org/img/wn/" + icon + ".png");
+nameData.text(cityName)
+tempData.text(temperature)
+windData.text(windspeed)
+humidData.text(humidity)
+
+    }
+
+    //console.log(temperature , cityName , windspeed , humidity, icon);
+
+
+
     
 var pastCityBtn = $("<button>").text(city)
 citysBtn.append(pastCityBtn)
@@ -34,12 +63,39 @@ pastCityBtn.click(() => weatherbtn(latValue, lonValue))
 })
 })
 
+
 function weatherbtn(lat, lon){
-    var weather = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=78da9311b6649705d4159cf6966655ff";
+    var weather = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&cnt=6&appid=78da9311b6649705d4159cf6966655ff"
     fetch(weather)
     .then((response => response.json()))
     .then(data => {
-        console.log(data)
+        forecast.empty()
+        for (var i = 1; i <= 5; i++){
+        var icon = data.list[i].weather[0].icon;
+        var cityName = data.city.name
+        var temperature = data.list[i].main.temp;
+        var windspeed = data.list[i].wind.speed;
+        var humidity = data.list[i].main.humidity;
+        var h3 = $('<h3>').text(cityName)
+        var p1 = $('<p>').text(temperature)
+        var p2 = $('<p>').text(windspeed)
+        var p3 = $('<p>').text(humidity)
+        var img = $('<img>').attr("src", "http://openweathermap.org/img/wn/" + icon + ".png")
+        var div = $('<div>').addClass("forecast-day").append(h3, img, p1, p2, p3)
+        forecast.append(div)
+    
+    
+    
+    
+    
+    iconData.attr("src", "http://openweathermap.org/img/wn/" + icon + ".png");
+    nameData.text(cityName)
+    tempData.text(temperature)
+    windData.text(windspeed)
+    humidData.text(humidity)
+    
+        }
+
     })
 }
 
