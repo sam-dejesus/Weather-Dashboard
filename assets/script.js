04c397d1fb73c6194067c418d47d53c37770a815
samuel de jesus <samue@MacBook-Air.local>
2023-04-02 12:28:22 -0400

    the application now stores var city value in local storage also the weather icon and 5 day forcast will show up with the use of a for loop

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,25 @@ var tempData = $('#tempofcity')
 var humidData = $('#humidityofcity')
 var iconData = $('#icons')
 var forecast = $('#forecast')
+var date = dayjs().format('M/DD/YY')
+var h4 = $('<h4>').text(date) 
 
+
+//
+var cityData = JSON.parse(localStorage.getItem('citydata')) || {};
+for (var key in cityData) {
+  var city = cityData[key].name;
+  var button = $('<button>').text(city);
+  citysBtn.append(button);
+
+}
+//
+
+
+
+var history = {
+
+}
 
 searchBtn.click(function go(){
  var city = $('#city-search').val();   
@@ -23,7 +41,7 @@ fetch(url)
  var weather = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latValue + "&lon=" + lonValue + "&units=imperial&cnt=6&appid=78da9311b6649705d4159cf6966655ff"
 fetch(weather)
 .then(response => response.json())
-.then(data => { //console.log(data);
+.then(data => { 
     forecast.empty()
     for (var i = 1; i <= 5; i++){
     var icon = data.list[i].weather[0].icon;
@@ -31,7 +49,7 @@ fetch(weather)
     var temperature = data.list[i].main.temp;
     var windspeed = data.list[i].wind.speed;
     var humidity = data.list[i].main.humidity;
-    var h3 = $('<h3>').text(cityName)
+    var h3 = $('<h3>').text(date)
     var p1 = $('<p>').text(temperature)
     var p2 = $('<p>').text(windspeed)
     var p3 = $('<p>').text(humidity)
@@ -41,23 +59,26 @@ fetch(weather)
 
 
 
-
-
 iconData.attr("src", "http://openweathermap.org/img/wn/" + icon + ".png");
 nameData.text(cityName)
 tempData.text(temperature)
 windData.text(windspeed)
 humidData.text(humidity)
 
-    }
+nameData.append(h4)
 
-    //console.log(temperature , cityName , windspeed , humidity, icon);
+}
 
 
 
-    
+
+
 var pastCityBtn = $("<button>").text(city)
 citysBtn.append(pastCityBtn)
+//
+cityData[cityName] = { name: cityName };
+localStorage.setItem('citydata', JSON.stringify(cityData));
+
 pastCityBtn.click(() => weatherbtn(latValue, lonValue))
 })
 })
@@ -99,3 +120,48 @@ function weatherbtn(lat, lon){
     })
 }
 
+
+var button = $('button');
+
+button.click(function recall() {
+ var recallName = $(this).text()
+ var recallUrl = "http://api.openweathermap.org/geo/1.0/direct?q= "+ recallName + "&limit=1&appid=78da9311b6649705d4159cf6966655ff"
+ fetch(recallUrl)
+.then(response => response.json())
+.then(data => {
+ var {lat, lon } = data[0]
+ var latValue = lat;
+ var lonValue = lon;
+ var weather = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latValue + "&lon=" + lonValue + "&units=imperial&cnt=6&appid=78da9311b6649705d4159cf6966655ff"
+fetch(weather)
+.then(response => response.json())
+.then(data => { //console.log(data);
+    forecast.empty()
+    for (var i = 1; i <= 5; i++){
+    var icon = data.list[i].weather[0].icon;
+    var cityName = data.city.name
+    var temperature = data.list[i].main.temp;
+    var windspeed = data.list[i].wind.speed;
+    var humidity = data.list[i].main.humidity;
+    var h3 = $('<h3>').text(date)
+    var p1 = $('<p>').text(temperature)
+    var p2 = $('<p>').text(windspeed)
+    var p3 = $('<p>').text(humidity)
+    var img = $('<img>').attr("src", "http://openweathermap.org/img/wn/" + icon + ".png")
+    var div = $('<div>').addClass("forecast-day").append(h3, img, p1, p2, p3)
+    forecast.append(div)
+
+
+
+iconData.attr("src", "http://openweathermap.org/img/wn/" + icon + ".png");
+nameData.text(cityName)
+tempData.text(temperature)
+windData.text(windspeed)
+humidData.text(humidity)
+
+nameData.append(h4)
+
+}
+
+})
+})})
